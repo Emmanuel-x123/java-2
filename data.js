a8d84bd8a9bcafdc6ea3b8f35d02a0cd2efcc49a
emmanuel-x123 <emmanuelreubenx123@gmail.com>
2024-03-24 10:14:01 -0700

    added html css and js file

---
+++ data.js
@@ -0,0 +1,51 @@
+// let Name = ''
+
+// if(typeof(Name === "")){
+//     prompt('this data is a string', typeof(Name))     
+// }else if(Name ===! ''){
+//     prompt('invalid data')
+// }
+
+// let number = Number()
+
+// if(typeof(number === Number())){
+//     prompt('this data is a number', typeof(number))  
+// }
+
+
+
+// document.getElementById('data').innerHTML = Name
+let userName = prompt('Enter your username');
+document.getElementById('username').innerHTML = ("username: " + userName);
+let cohort = prompt('Enter your cohort');
+document.getElementById('cohort').innerHTML =("cohort: " + cohort);
+let pass_Code = prompt('Enter your Passcode');
+document.getElementById('passcode').innerHTML = ("passcode: " + pass_Code);
+
+let Name = prompt('entar a data')
+let age = parseInt(prompt('enter a data'), Number())
+
+if(typeof(Name === '')){
+    prompt('this data is a string', typeof(Name))   
+}
+
+if(typeof(age === 3)){
+   
+    prompt('this data is a number', typeof(age)) 
+}
+
+ document.getElementById('data').innerHTML ='this is a string: ' + Name
+ document.getElementById('age').innerHTML ='this is a number: ' + age
+
+
+
+
+
+
+
+
+
+
+
+
+
